e3a2dd80a613a03f0e5c0c08f8444ec8d75025a6
wendbenindo <internetdg27@gmail.com>
2025-10-17 05:27:48 +0000

    fix of the gradle ndkversion error

--- agri_desease_detect_app/android/app/build.gradle.kts
+++ agri_desease_detect_app/android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.example.agri_desease_detect_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
